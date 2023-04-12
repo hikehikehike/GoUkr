d5fbdf4273b2b8f2dfb738195b7334b5fcf4e318
Timur <timur.hike@gmail.com>
2023-04-12 12:11:31 +0300

    Add model City

--- app/models.py
+++ app/models.py
@@ -1,3 +1,21 @@
-from django.db import models
+import os
+import uuid
 
-# Create your models here.
+from django.db import models
+from django.utils.text import slugify
+
+
+def city_image_file_path(instance, filename):
+    _, extension = os.path.splitext(filename)
+    filename = f"{slugify(instance.name)}-{uuid.uuid4()}{extension}"
+
+    return os.path.join("uploads/city/", filename)
+
+
+class City(models.Model):
+    name = models.CharField(max_length=255)
+    song_line = models.TextField()
+    description = models.TextField()
+    gallery_image = models.ImageField(null=True, upload_to=city_image_file_path)
+    drive_url = models.URLField()
+
