21e1ef8922e3a55c6dd11d8c67556092e7c24508
Timur <timur.hike@gmail.com>
2023-04-12 23:22:59 +0300

    Add urls in app

---
+++ app/urls.py
@@ -0,0 +1,17 @@
+from django.urls import path, include
+from rest_framework import routers
+
+from app.views import CityViewSet
+
+router = routers.DefaultRouter()
+router.register("city", CityViewSet, basename="city")
+
+
+urlpatterns = [
+    path("", include(router.urls)),
+    path("cities/<str:slug>/", include(router.urls)),
+    path("cities/<str:slug>/route/", CityViewSet.as_view({"get": "route"}), name="city-route"),
+    path("cities/<str:slug>/hotels/", CityViewSet.as_view({"get": "hotels"}), name="city-hotels"),
+]
+
+app_name = "app"
