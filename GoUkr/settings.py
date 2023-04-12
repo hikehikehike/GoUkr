be86ce5e1bbf7d7e8c0a0fec30dfd261b0a3377e
Timur <timur.hike@gmail.com>
2023-04-12 14:16:40 +0300

    Add media

--- GoUkr/settings.py
+++ GoUkr/settings.py
@@ -38,7 +38,6 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "rest_framework",
-    "multifilefield",
     "app",
     "user",
 ]
@@ -121,6 +120,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "static/"
+
+MEDIA_URL = "/media/"
+MEDIA_ROOT = BASE_DIR / "media"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
