1cd5fc1603d121a437600e3f1ed65e077fe1ab45
Timur <timur.hike@gmail.com>
2023-04-19 17:02:19 +0300

    Fix bag media

--- app/parse.py
+++ app/parse.py
@@ -62,8 +62,13 @@ def parse_hotels(city):
 
 def parse_restaurant(city):
     headers = {
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3",
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.101 Safari/537.36",
+        "Accept-Language": "en-US,en;q=0.9",
+        "Accept-Encoding": "gzip, deflate, br",
         "Referer": "https://www.google.com/",
+        "Connection": "keep-alive",
+        "Cache-Control": "max-age=0",
+        "TE": "Trailers",
     }
     url = f"https://restaurantguru.com/{city.name}"
     page = requests.get(url, headers=headers).content
