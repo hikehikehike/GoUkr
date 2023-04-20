5b0c301649eb5fd8ca9f11ca7aabc0e34e6ef388
Timur <timur.hike@gmail.com>
2023-04-20 21:27:17 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,6 +2,11 @@
 # exit on error
 set -o errexit
 
+# create temporary directory for apt
+mkdir -p /tmp/lists/partial
+mkdir -p /var/lib/apt/lists/partial
+mount --bind /tmp/lists/partial /var/lib/apt/lists/partial
+
 # install dependencies
 apt-get update
 apt-get install -y wget unzip libglib2.0-0 libnss3 libgconf-2-4 libfontconfig1
@@ -14,3 +19,7 @@ pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 python manage.py migrate
+
+# unmount temporary directory
+umount /var/lib/apt/lists/partial
+rm -rf /tmp/lists
