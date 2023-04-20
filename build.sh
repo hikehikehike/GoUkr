e9d944cb71f287e27ec68412f9bcc2f83b379987
Timur <timur.hike@gmail.com>
2023-04-20 21:31:12 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,14 +2,9 @@
 # exit on error
 set -o errexit
 
-# create temporary directory to avoid permission issues
-mkdir /tmp/apt && mkdir /tmp/apt/lists && mkdir /tmp/apt/archives && mkdir /tmp/apt/partial
-
-# update and install packages
-apt-get update -o Dir::Etc::sourcelist=/dev/null \
--o Dir::Etc::sourceparts=/tmp/apt/lists \
--o APT::Get::List-Cleanup='false'
-apt-get install -y wget unzip libglib2.0-0 libnss3 libgconf-2-4 libfontconfig1
+# install dependencies
+sudo apt-get update
+sudo apt-get install -y wget unzip libglib2.0-0 libnss3 libgconf-2-4 libfontconfig1
 
 # download and install ChromeDriver
 wget https://chromedriver.storage.googleapis.com/94.0.4606.61/chromedriver_linux64.zip
